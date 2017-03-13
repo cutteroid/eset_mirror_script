25a086bd7553a20196dd96e74b916671faa7ceab
Harmless <harmless@syneforge.com>
2017-03-13 17:07:05 +0200

    Set treads to 25

--- inc/classes/Mirror.class.php
+++ inc/classes/Mirror.class.php
@@ -228,7 +228,7 @@ class Mirror {
     if ($mirror != null) {
      if (function_exists('curl_multi_init')){
       $test = false;
-      $treads = 1;
+      $treads = 25;
       $file = array();
       $treads = (count($download_files) < $treads) ? count($download_files) : $treads;
       $master = curl_multi_init();
