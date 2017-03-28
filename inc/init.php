65da1823061f3dc164db607515ecf8cea5bc00d6
harmless <harmless@syneforge.com>
2017-03-29 02:12:55 +0300

    Fix script version

--- inc/init.php
+++ inc/init.php
@@ -22,7 +22,7 @@ $SELFUPDATE_POSTFIX = array(
 );
 
 define('DS', DIRECTORY_SEPARATOR);
-define('VERSION', '1.0.20190328 [Freedom for Ukraine][Moded by harmless]');
+define('VERSION', '1.0.20190329 [Freedom for Ukraine][Moded by harmless]');
 define('SELF', substr(dirname(__FILE__),0,-3)); 
 define('INC', substr(dirname(__FILE__),0,-3)."inc".DS);
 define('CLASSES', substr(dirname(__FILE__),0,-3)."inc".DS."classes".DS);
