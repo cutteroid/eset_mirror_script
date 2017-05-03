6d7a4a479d0cf5aaa8171dd8c86babe7cd4e432e
Harmless <harmless@syneforge.com>
2017-05-03 16:25:00 +0300

    Disabled exit script execution when it was updated

--- update.php
+++ update.php
@@ -22,8 +22,8 @@ if (Config::get('self_update') > 0) {
    if (Config::get('self_update') > 1) {
     SelfUpdate::start_to_update();
     Log::informer(Language::t("Your script has been successfully updated to version %s!",SelfUpdate::get_version_on_server()), null, 0);
-    Log::destruct();
-    exit;
+    //Log::destruct();
+    //exit;
    }
   } else
       Log::write_log(Language::t("You already have actual version of script! No need to update!"), 0);
